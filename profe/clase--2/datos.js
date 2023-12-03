0e3f4910b6c99c91edc13d76f7352684df2a5d0b
Francoparodi01 <francoparodi2001@gmail.com>
2023-12-03 16:12:03 -0300

    Configurando gitignore

--- profe/clase--2/datos.js
+++
@@ -1,9 +0,0 @@
-const productosMarolio = [
-    {id: 1, nombre: "mate", precio: 500},
-    {id: 2, nombre: "cafe", precio: 700},
-    {id: 3, nombre: "harina", precio: 800},
-    {id: 4, nombre: "palmitos", precio: 900},
-]
-
-export default productosMarolio;
-
